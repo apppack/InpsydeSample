8cb65e712b761dd1129e8b46392a0f59234b5a94
Hemang Vyas <apppack1@gmail.com>
2016-09-02 19:44:44 +0300

    Update samplenonce-test.php

--- tests/samplenonce-test.php
+++ tests/samplenonce-test.php
@@ -1,10 +1,5 @@
 <?php 
 
-$WP_ROOT = "C:\Program Files (x86)\Ampps\www\inpsydesample";
-require( $WP_ROOT . '/wp-blog-header.php' );
-
-require_once dirname( __FILE__ ) . '/../samplenonce.php';
-
 
 
 class NonceTest extends PHPUnit_Framework_TestCase {
