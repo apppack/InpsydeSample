8af41370c8f11a72758dd595b583e1c770176ab9
Hemang Vyas <apppack1@gmail.com>
2016-09-02 20:05:31 +0300

    Update samplenonce-test.php

--- tests/samplenonce-test.php
+++ tests/samplenonce-test.php
@@ -8,6 +8,12 @@ class NonceTest extends PHPUnit_Framework_TestCase {
 public function testWpCreateNonce() {
 
 $InpCreateNonce = new  InpCreateNonce();
+
+\WP_Mock::wpFunction( 'wp_create_nonce', array(
+				'times'  => 1,
+				'return' => $InpCreateNonce
+			) );
+
 $this->assertNotNull( $InpCreateNonce );
 }
 }
