243a19c2a3114dbb323f9aad9db1923cd41eb82b
Hemang Vyas <apppack1@gmail.com>
2016-09-03 12:34:19 +0300

    Update InpsydeWPNonce-test.php

--- tests/InpsydeWPNonce-test.php
+++ tests/InpsydeWPNonce-test.php
@@ -5,8 +5,6 @@
  *
  */
 
-
-
 class InpNonceTest extends PHPUnit_Framework_TestCase {
 	public function setUp() {
 		\WP_Mock::setUp();
@@ -14,8 +12,14 @@ class InpNonceTest extends PHPUnit_Framework_TestCase {
 	public function tearDown() {
 		\WP_Mock::tearDown();
 	}
+	
+/*
+ * TESTS for wp_create_nonce()
+ *
+ */
+ 
 public function testWpCreateNonce() {
-$action = 'my_new_action';
+$action = 'inpsyde_new_action';
 $nonce = 'inp_nonce';
 
 $InpNonces = new  InpNonces($action);
@@ -30,4 +34,50 @@ $this->assertEquals(
 	$InpNonces ->  InpCreateNonce() 
 	);
 }
+
+/*
+ * TESTS for wp_create_nonce()
+ *
+ */
+ 
+public function testWpCreateNonce() {
+$action = 'inpsyde_new_action';
+$nonce = 'inp_nonce';
+
+$InpNonces = new  InpNonces($action);
+
+\WP_Mock::wpFunction( 'wp_create_nonce', array(
+				'times'  => 1,
+				'return' => $nonce
+			) );
+
+$this->assertEquals( 
+	$nonce,
+	$InpNonces ->  InpCreateNonce() 
+	);
+}
+
+/*
+ * TESTS for wp_create_nonce()
+ *
+ */
+ 
+public function testWpCreateNonce() {
+$action = 'inpsyde_new_action';
+$nonce = 'inp_nonce';
+
+$InpNonces = new  InpNonces($action);
+
+\WP_Mock::wpFunction( 'wp_verify_nonce', array(
+				'times'  => 1,
+				'return' => $nonce
+			) );
+
+$this->assertEquals( 
+	$nonce,
+	$InpNonces ->  InpVerifyNonce($nonce) 
+	);
+}
+
+
 }
