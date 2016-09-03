acfff97237d54e5b4673c8ae719bef2ca5b6a7f1
Hemang Vyas <apppack1@gmail.com>
2016-09-03 12:34:19 +0300

    Update InpsydeWPNonce.php

--- InpsydeWPNonce.php
+++ InpsydeWPNonce.php
@@ -15,7 +15,7 @@ abstract class AbsInpNonces {
         if (empty($action)) {
     $action = 'inpsyde_action';
 } else {
-    $action = -1;
+    $action = '-1';
 }
     }
     
@@ -24,19 +24,61 @@ abstract class AbsInpNonces {
         return $this->action;
     }
 
+	/**
+	 * Generates and returns a nonce. The nonce is generated based on the current time, the $action argument, and the current user ID.
+	 * @param  Integer $action Action name. Default: -1.
+	 * @return String          The one use form token.
+	 * see https://developer.wordpress.org/reference/functions/wp_create_nonce/
+	 */
+	 
 	public function InpCreateNonce() {
 	
 		return wp_create_nonce($this -> action);
+		
+	}
+	
+	/**
+	 * Verify that a nonce is correct and unexpired with the respect to a specified action.
+	 * @param  String $nonce   Nonce to verify.
+	 * @param  Integer $action Action name. Default: -1.
+	 * @return Boolean/Integer False if the nonce is invalid, 1 – if the nonce has been generated in the past 12 hours or less., 2 – if the nonce was generated between 12 and 24 hours ago.
+	 * see https://developer.wordpress.org/reference/functions/wp_verify_nonce/
+	 */
+	 
+	public function InpVerifyNonce($nonce) {
+	
+		return wp_verify_nonce($nonce, $this -> action);
 		
 	}
 }
 
 class InpNonces extends AbsInpNonces {
 	
+	/**
+	 * Generates and returns a nonce. The nonce is generated based on the current time, the $action argument, and the current user ID.
+	 * @param  Integer $action Action name. Default: -1.
+	 * @return String          The one use form token.
+	 * see https://developer.wordpress.org/reference/functions/wp_create_nonce/
+	 */
+	 
 	public function InpCreateNonce() {
 	
 	return wp_create_nonce($this -> action);
 	
+	}
+	
+	/**
+	 * Verify that a nonce is correct and unexpired with the respect to a specified action.
+	 * @param  String $nonce   Nonce to verify.
+	 * @param  Integer $action Action name. Default: -1.
+	 * @return Boolean/Integer False if the nonce is invalid, 1 – if the nonce has been generated in the past 12 hours or less., 2 – if the nonce was generated between 12 and 24 hours ago.
+	 * see https://developer.wordpress.org/reference/functions/wp_verify_nonce/
+	 */
+	 
+	public function InpVerifyNonce($nonce) {
+	
+		return wp_verify_nonce($nonce, $this -> action);
+		
 	}
 	
 }
