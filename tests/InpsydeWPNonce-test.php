b4926a4fa845338ed46aac66765ea97709b6a9a4
Hemang Vyas <apppack1@gmail.com>
2016-09-05 11:13:33 +0300

    Update InpsydeWPNonce-test.php

--- tests/InpsydeWPNonce-test.php
+++ tests/InpsydeWPNonce-test.php
@@ -44,7 +44,7 @@ $this->assertEquals(
 public function testWpVerifyNonce() {
 $action = 'inpsyde_test_action';
 $nonce = 'inpsyde_test_nonce';
-$InpNonces = new  InpNonces($nonce . 'inpsyde_nonce_failed', $action);
+$InpNonces = new  InpNonces('inpsyde_nonce_failed', $action);
 
 \WP_Mock::wpFunction( 'wp_verify_nonce', array(
 				'times'  => 1,
