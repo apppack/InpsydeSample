67313263fce6a1878479708213edc860e295aec6
Hemang Vyas <apppack1@gmail.com>
2016-09-05 10:54:48 +0300

    Update InpsydeWPNonce-test.php

--- tests/InpsydeWPNonce-test.php
+++ tests/InpsydeWPNonce-test.php
@@ -58,7 +58,7 @@ $this->assertEquals(
 
 $this->assertNotEquals( 
 	$nonce,
-	$InpNonces -> InpVerifyNonce($nonce.'_failed', $action)
+	$InpNonces -> InpVerifyNonce(inpsyde_nonce_failed, $action)
 	);
 }
 
