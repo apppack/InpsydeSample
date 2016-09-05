3d84aad51aaef2f02b793dbea10bd31866b881e7
Hemang Vyas <apppack1@gmail.com>
2016-09-05 11:07:30 +0300

    Update InpsydeWPNonce-test.php

--- tests/InpsydeWPNonce-test.php
+++ tests/InpsydeWPNonce-test.php
@@ -58,7 +58,7 @@ $this->assertEquals(
 
 $this->assertNotEquals( 
 	$nonce,
-	$InpNonces -> InpVerifyNonce( $nonce . 'inpsyde_nonce_failed')
+	($action, ($InpNonces -> InpVerifyNonce( $nonce . 'inpsyde_nonce_failed')))
 	);
 }
 
