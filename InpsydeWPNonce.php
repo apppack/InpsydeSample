f7e6303a95c0939b713ce0e3897d9f4dca18cc52
Hemang Vyas <apppack1@gmail.com>
2016-09-05 14:11:55 +0300

    Update InpsydeWPNonce.php

--- InpsydeWPNonce.php
+++ InpsydeWPNonce.php
@@ -87,7 +87,7 @@ class InpNonces extends AbsInpNonces {
 	 * see https://developer.wordpress.org/reference/functions/wp_nonce_field/
 	 */
 	 
-	public function InpNonceField( $action, $name = '_inpnonce', $referer = true, $echo = true) {
+	public function InpNonceField( $action, $name = '_inpnonce', $referer = true, $echo = false) {
 		
 		return wp_nonce_field($this -> action, $name, $referer, $echo);
 
